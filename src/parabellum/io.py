72fd2b294cf08f7c49a0e282245aa83f304b66bb
Felix Lenner <felix.lenner@scilifelab.se>
2026-01-27 11:22:35 +0100

    Add Flagged column to tsv output

--- src/parabellum/io.py
+++ src/parabellum/io.py
@@ -57,24 +57,24 @@ def print_tsv(json_data: Dict) -> List[Tuple[str, str, str, float, float]]:
 
     Where Value sometimes is a numeric, sometimes a list and sometimes a dict.
     """
-    flagged = []
 
     for sample, genes in json_data.items():
         for gene, gene_info in genes.items():
+            key_is_flagged = False
             for key, val in gene_info.items():
                 if isinstance(val, dict) and {"value", "normal", "flag"}.issubset(val):
                     value_str = stringify_value(val["value"])
-                    print(f"{sample}\t{gene}\t{key}\t{value_str}")
-
                     # Flag in Scout
                     if val["flag"]:
+                        key_is_flagged = True
                         logger.info(
                             f"Flagging region {gene} in sample {sample} because of key {key} (value: {val['value']}, normal: {val['normal']})"
                         )
+                    print(f"{sample}\t{gene}\t{key_is_flagged}\t{key}\t{value_str}")
                 else:
-                    print(f"{sample}\t{gene}\t{key}\t{stringify_value(val)}")
-
-    return flagged
+                    print(
+                        f"{sample}\t{gene}\t{key_is_flagged}\t{key}\t{stringify_value(val)}"
+                    )
 
 
 def stringify_value(value) -> str | None:
